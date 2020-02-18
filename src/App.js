4a25713f1a6bfe123746466e9d19a664811b4099
leyuri <leyuri97@gmail.com>
2020-02-18 21:44:10 +0900

    Add genres

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from "axios";
 //axios.. 겉면의 layer와 같은...
 import Movie from "./Movie";
+import "./App.css";
 
 
 class App extends React.Component {
@@ -10,7 +11,7 @@ class App extends React.Component {
     movies: []
   };
 
-  getMovies = async () => {
+  getMovies = async ( ) => {
     const {
       data: {
         data: { movies }
@@ -26,20 +27,29 @@ class App extends React.Component {
 
   render(){
     const { isLoading, movies } = this.state;
-    return <div>{isLoading 
-      ? "Loading..." 
-      : movies.map(movie => (
-          <Movie 
-            key={movie.id}
-            id = {movie.id}
-            year = {movie.year}
-            title = {movie.title}
-            summary = {movie.summary}
-            poster = {movie.medium_cover_image}
-            />
-        ))}
-    </div>;
-  }
+    return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              id = {movie.id}
+              year = {movie.year}
+              title = {movie.title}
+              summary = {movie.summary}
+              poster = {movie.medium_cover_image}
+              genres = {movie.genres}
+              />
+            ))}
+          </div>
+        )}
+    </section>
+    )}
 }
 
 
