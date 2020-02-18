6c47f2b7c6f6ac4a5e7f539ac0422f81c2a5b24f
leyuri <leyuri97@gmail.com>
2020-02-18 22:12:31 +0900

    Updates

---
+++ precache-manifest.67e5b1ee887c034e120bf8042369fef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd45e03dfef6132343eb1f0ff86f772",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "4d0841e26ef774f2c9ef",
+    "url": "/movieapp/static/css/main.aa994cf1.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movieapp/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movieapp/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0841e26ef774f2c9ef",
+    "url": "/movieapp/static/js/main.af849398.chunk.js"
+  },
+  {
+    "revision": "f042a465717bdc2a30bf",
+    "url": "/movieapp/static/js/runtime-main.2c29b39e.js"
+  }
+]);
