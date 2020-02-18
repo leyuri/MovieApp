ce8d96f03294d23e7f83e80ce863b465b7bdc1f0
leyuri <leyuri97@gmail.com>
2020-02-18 16:43:11 +0900

    Add YTS API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import axios from "axios";
+//axios.. 겉면의 layer와 같은...
 
-function App() {
-    return <div>yuri</div>;
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const movies = axios.get("https://yts-proxy.now.sh/list_movies.json")
+  };
+
+  componentDidMount() {
+    this.getMovies();
+ }
+
+  render(){
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
+}
+
 
 export default App;
+ 
